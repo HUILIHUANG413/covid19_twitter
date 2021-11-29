ba81d6f88c27ea04b5422018f3243dbc16d5431d
Yuxuan Wang <nickwang_0226@berkeley.edu>
2021-11-29 17:00:31 -0500

    filtered

--- processing_code/Top20filter.py
+++ processing_code/Top20filter.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import os
 import sys
-from textblob import TextBlob
 from langdetect import detect
 
 
-pathN = "D:/Fall2021/CSE6242" + "/" + "6242Project/covid19_twitter/dailies/"
-ori_savePath ="D:/Fall2021/CSE6242" + "/" + "6242Project/covid19_twitter/" + "Top20_daily/"
+pathN = "D:/COVID_PROJ" + "/" + "covid19_twitter/dailies/"
+ori_savePath ="D:/COVID_PROJ" + "/" + "covid19_twitter/" + "Top30_daily/"
 
 # def is_english(text):
 #     # Add language detection code here
@@ -24,18 +23,52 @@ directory = os.fsencode(pathN)
 # arr = os.listdir()
 # print(arr)
 
-
+count = 0
 #
 for file in os.listdir(directory):
-     file_tmp = str(file)
-     file_tmp = file_tmp[2:-1]
-     foldername = pathN + file_tmp + "/"
-     for subfile in os.listdir(foldername):
+
+    #select dates
+
+    # count += 1 
+    # if count < 300 :
+    #     continue
+    # if count > 310 :
+    #     break
+    
+    file_tmp = str(file)
+    file_tmp = file_tmp[2:-1]
+    foldername = pathN + file_tmp + "/"
+    
+
+    for subfile in os.listdir(foldername):
         filename = os.fsdecode(subfile)
         if filename.endswith("trigrams.csv"):
-            data = pd.read_csv(foldername + filename, header=None,names=['gram','counts'],skiprows = 0, nrows =30)
+            data = pd.read_csv(foldername + filename, header=None,names=['gram','counts'],skiprows = 0, nrows =200)
             data = data[1:]
-            savePath = ori_savePath + filename[:-4]+ "_top20.csv"
+            
+            drop_index = []
+            for index, row in data.iterrows():
+                # filter repeated covid/19 combo
+                if row["gram"] != "covid 19":
+                    if "covid" in row["gram"] or "Covid" in row["gram"] or "19" in row["gram"] or "corona" in row["gram"]:
+                        drop_index.append(index)
+                #filter ?
+                if "?" in row["gram"]:
+                    drop_index.append(index)
+                #filter non-English
+                try:
+                    language = detect(row["gram"])
+                except:
+                    language = "error"
+                if (row["gram"] != "covid 19" and  "vaccine" not in row["gram"] and language != "en"):
+                    drop_index.append(index)
+                #filter big numbers like ,000
+                if ("000" in row["gram"] or "people" in row["gram"]):
+                    drop_index.append(index)
+
+            data.drop(labels = drop_index, axis = 0, inplace = True)
+            savePath = ori_savePath + filename[:-4]+ "_top30.csv"
             data.to_csv(savePath,index=False)
+            print(filename[:-4])
         else:
             continue
